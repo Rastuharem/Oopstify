e3e38a28802687a2f25388b133fc69b86389115e
Rastuharem <Rastuharem@yandex.ru>
2022-05-03 13:29:23 +0300

    changed inputs

--- src/components/UploadSongForm.jsx
+++ src/components/UploadSongForm.jsx
@@ -1,96 +1,149 @@
 import React, { useEffect, useState } from "react";
-import "../styles/UploadSongForm.css"
+import "../styles/UploadSongForm.css";
 import { UploadSvg } from "../svg";
-import songs from "../data/songs.json"
-
-
+import songs from "../data/songs.json";
 
 const UploadSongForm = () => {
-    const newSong = Object.assign({}, songs[0]);
+  const newSong = Object.assign({}, songs[0]);
 
+  const [UploadedFile, setUploadedFile] = useState(null);
+
+  const [NewSongTitle, setNewSongTitle] = useState("");
+  const [NewSongAuthor, setNewSongAuthor] = useState("");
+  const [NewSongAuthorImg, setNewSongAuthorImg] = useState("");
+  const [NewSongAlbumImg, setNewSongAlbumImg] = useState("");
+
+  useEffect(() => {
     newSong.name = "";
     newSong.author = "";
     newSong.id = 0;
     newSong.url = "";
     newSong.links.images[0] = "";
     newSong.links.images[1] = "";
+  }, []);
 
-    let SongFields = [];
-    let [authorImg, albumImg] = [];
-    const [FieldInputs, setFieldInputs] = useState();
-
-    const showUploadedFile = () => {
-        const input = document.getElementById("input__file");
-        const label = document.getElementById("FilePath");
-        if (input.files.length !== 1) {
-            alert("You can upload only one file at a time!");
-        } else {
-            const file = input.files[0];
-            console.log(file);
-            label.innerText = file.name;
-            showSongOption();
-        }
+  const showUploadedFile = () => {
+    const input = document.getElementById("input__file");
+    if (input.files.length !== 1) {
+      alert("You can upload only one file at a time!");
+    } else {
+      setUploadedFile(input.files[0]);
+      document.getElementById("FilePath").innerText = input.files[0].name;
+      document.getElementById("SendBtn").style.display='inline';
     }
+  };
 
-    const showSongOption = () => {
-        for (let key in newSong) {
-            SongFields.push([key, newSong[key]]);
-        }
-        console.log(SongFields[0][0]);
-        authorImg = SongFields[SongFields.length-1][1]["images"][0];
-        albumImg = SongFields[SongFields.length-1][1]["images"][1];
-        SongFields[SongFields.length-1]=['authorImg', authorImg];
-        SongFields.push(['albumImg', albumImg]);
-        setFieldInputs(SongFields.map((index)=>{
-          return (
-            <div key={index}>
-              <h2>{SongFields[index]}: </h2>
-              <input value={SongFields[index][1]}></input>
-            </div>
-          )}));
-    }
+  const CreateNewSong = (e) => {
+    e.preventDefault();
+    if (NewSongTitle==="" || NewSongAuthor==="") {
+      alert("Please, fill required inputs!")
+    } else {
 
-    
+
+
+      newSong.name = NewSongTitle;
+      newSong.author = NewSongAuthor;
+      newSong.id = songs[songs.length-1]["id"] + 1;
+      newSong.links.images[0] = NewSongAuthorImg;
+      newSong.links.images[1] = NewSongAlbumImg;
+      newSong.url= ;
+      console.log(newSong);
       
-    return(
-        <form
-          action="/upload"
-          method="post"
-          encType="multipart/form-data"
-          className="musicForm"
-        >
-          <div className="input__wrapper">
-            <input
-              name="file"
-              type="file"
-              id="input__file"
-              className="input input__file"
-              multiple
-              onChange={showUploadedFile}
-            />
-            <label htmlFor="input__file" className="input__file-button">
-              <span className="input__file-icon-wrapper">
-                  <div className="svgContainer">
-                {UploadSvg}
-                </div>
-              </span>
-              <span className="input__file-button-text">Choose file</span>
-            </label>
-          </div>
-            <div className="songInfoContainer">
-                <div className="filePathContainer">
-                    <h1>Selected file: </h1>
-                    <label id="FilePath"></label>
-                </div>
-                <div className="songFieldsContainer">
-                    {FieldInputs}
-                </div>
-            </div>
-          <div className="senderContainer">
-            <input type="submit" value="Upload" id="SendBtn" hidden={false} />
-            </div>
-        </form>
-    );
+    }
+  }
+
+  return (
+    <form
+      action="/upload"
+      method="post"
+      encType="multipart/form-data"
+      className="musicForm"
+    >
+      <div className="input__wrapper">
+        <input
+          name="file"
+          type="file"
+          id="input__file"
+          className="input input__file"
+          multiple
+          onChange={showUploadedFile}
+        />
+        <label htmlFor="input__file" className="input__file-button">
+          <span className="input__file-icon-wrapper">
+            <div className="svgContainer">{UploadSvg}</div>
+          </span>
+          <span className="input__file-button-text">Choose file</span>
+        </label>
+      </div>
+      <div className="songInfoContainer">
+        <div className="filePathContainer">
+          <h1>Selected file: </h1>
+          <label id="FilePath"></label>
+        </div>
+        <div className="songFieldsContainer">
+
+          {UploadedFile !== null ? (
+            <>
+              <div className="inputContainer">
+                <label id="SongTitleLabel">Type title here: </label>
+                <input
+                  type="text"
+                  id="SongTitle"
+                  className="fieldInput"
+                  value={NewSongTitle}
+                  onChange={(event) => setNewSongTitle(event.target.value)}
+                  placeholder="Required"
+                />
+              </div>
+              <div className="inputContainer">
+                <label id="SongAuthorLabel">Type author here: </label>
+                <input
+                  type="text"
+                  id="SongAuthor"
+                  className="fieldInput"
+                  value={NewSongAuthor}
+                  onChange={(event) => setNewSongAuthor(event.target.value)}
+                  placeholder="Required"
+                />
+              </div>
+              <div className="inputContainer">
+                <label id="SongAuthorImgLabel">
+                  Type link to author img here:{" "}
+                </label>
+                <input
+                  type="text"
+                  id="SongAuthorImg"
+                  className="fieldInput"
+                  value={NewSongAuthorImg}
+                  onChange={(event) => setNewSongAuthorImg(event.target.value)}
+                  placeholder="Can be empty"
+                />
+              </div>
+              <div className="inputContainer">
+                <label id="SongAlbumImgLabel">
+                  Type link to album img here:{" "}
+                </label>
+                <input
+                  type="text"
+                  id="SongAlbumImg"
+                  className="fieldInput"
+                  value={NewSongAlbumImg}
+                  onChange={(event) => setNewSongAlbumImg(event.target.value)}
+                  placeholder="Can be empty"
+                />
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
+
+        </div>
+        <div className="senderContainer">
+          <button type="submit" id="SendBtn" onClick={CreateNewSong}>Upload song</button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default UploadSongForm;
